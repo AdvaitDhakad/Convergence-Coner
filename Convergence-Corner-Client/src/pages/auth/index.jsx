98b63a34038b279ee204150de6ccc90d9f93e37f
Advait Dhakad <advaitdhakad@gmail.com>
2024-10-18 00:39:40 +0530

    added shadecn login components

--- Convergence-Corner-Client/src/pages/auth/index.jsx
+++ Convergence-Corner-Client/src/pages/auth/index.jsx
@@ -1,3 +1,8 @@
+import Background from "@/assets/login2.png";
+import Victory from "@/assets/victory.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+
 const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
@@ -10,8 +15,21 @@ const Auth = () => {
             <div className="flex items-center justify-center">
               <h1 className="text-5xl font-bold md:text-6xl text-blue-500">
                 Convergence Corner</h1>
+                <img src={Victory} alt="Victory Emogji" className="h-[100px]" />
             </div>
+            <p className="font-medium text-center">
+              Fill in the details
+            </p>
           </div>
+          <Tabs defaultValue="account" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="account">Account</TabsTrigger>
+    <TabsTrigger value="password">Password</TabsTrigger>
+  </TabsList>
+  <TabsContent value="account">Make changes to your account here.</TabsContent>
+  <TabsContent value="password">Change your password here.</TabsContent>
+</Tabs>
+
         </div>
       </div>
     </div>
